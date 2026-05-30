f6089d85e149461b53241c02a18e342671fec3a9
Nguyen Thanh An <73547738+thanhan92-f1@users.noreply.github.com>
2026-05-31 01:25:20 +0700

    Create english.php

---
+++ lang/english.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('WHMCS')) {
+    die('This file cannot be accessed directly');
+}
+
+$_ADDONLANG['title'] = 'Viettel SInvoice';
+$_ADDONLANG['description'] = 'Issue Vietnamese electronic invoices through Viettel SInvoice.';
+$_ADDONLANG['poll_pending'] = 'Poll Pending Records';
+$_ADDONLANG['issue'] = 'Issue';
+$_ADDONLANG['poll'] = 'Poll';
+$_ADDONLANG['retry'] = 'Force Retry';
+$_ADDONLANG['invoice_id'] = 'Invoice ID';
+$_ADDONLANG['status_sent'] = 'Sent';
+$_ADDONLANG['status_async'] = 'Pending Reconciliation';
+$_ADDONLANG['status_error'] = 'Error';
+$_ADDONLANG['not_configured'] = 'Module is not fully configured.';
